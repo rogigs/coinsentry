5bd254ef260d0edc9fa6d9f16fafe6986ed919ae
Igor Soares <igorgomes.13ig@gmail.com>
2023-08-02 19:34:44 -0300

    style: change style texts of component Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,34 @@
 import CardMUI from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { CardActionArea } from "@mui/material";
+import styled from "styled-components";
+import media from "../../styledMedia";
+
+const CardStyled = styled(CardMUI)`
+  width: 300px;
+  text-align: center;
+
+  ${media.lessThan("tablet")`
+    width: 100%
+`}
+`;
 
 const Card = ({ type, value }) => {
   return (
-    <CardMUI sx={{ width: 300 }}>
+    <CardStyled>
       <CardActionArea>
-        <CardContent>
+        <CardContent
+          sx={{
+            "&::first-letter": {
+              textTransform: "uppercase",
+            },
+          }}
+        >
           <h1>{type}</h1>
           <h2>{value}</h2>
         </CardContent>
       </CardActionArea>
-    </CardMUI>
+    </CardStyled>
   );
 };
 
