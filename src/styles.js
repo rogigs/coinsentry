4f41f3afd2b217f919136ddaf6f15fbb2141c504
Igor Soares <igorgomes.13ig@gmail.com>
2023-08-02 19:35:38 -0300

    style: add new wrappers in App Home

--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,27 @@
 import styled from "styled-components";
+import media from "./styledMedia";
 
-export const WrappperCard = styled.main`
+export const Wrappper = styled.section`
+  padding: 24px 0;
+
+  ${media.lessThan("tablet")`
+  padding: 24px;
+`}
+`;
+export const WrappperForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 12px;
+  margin-bottom: 24px;
+`;
+
+export const WrappperCard = styled.article`
   display: flex;
   width: 100%;
+  flex-wrap: wrap;
   justify-content: space-between;
+  row-gap: 24px;
+  margin-bottom: 24px;
+  margin-bottom: 24px;
 `;
