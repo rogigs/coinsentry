116bcdfe4d4637b07103d9f3b3c39621c90609d8
Igor Soares <igorgomes.13ig@gmail.com>
2023-08-03 03:27:41 -0300

    test: add tests for component Card

---
+++ src/components/Card/Card.spec.js
@@ -0,0 +1,25 @@
+import Card, { TYPES } from ".";
+import { screen, render, cont } from "@testing-library/react";
+import { TYPES_COLORS } from "./utils";
+
+describe("<Card />", () => {
+  it("should render props texts correctly", () => {
+    const props = { type: TYPES.ENTRADA, value: 20 };
+    render(<Card {...props} />);
+
+    expect(screen.getByText(props.type)).toBeInTheDocument();
+    expect(screen.getByText(`R$ ${props.value}`)).toBeInTheDocument();
+  });
+
+  it.todo(
+    "should render card of type TOTAL bigger than 0 with the correct colors"
+  );
+
+  it.todo(
+    "should render card of type TOTAL less than 0 with the correct colors"
+  );
+
+  it.todo(
+    "should render card of type TOTAL is equal 0 with the correct colors"
+  );
+});
