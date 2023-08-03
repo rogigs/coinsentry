0cfbd6cef2932f7398ea53ccb7329ac871caf6ea
Igor Soares <igorgomes.13ig@gmail.com>
2023-08-02 23:09:06 -0300

    feat: add routes insertItem and deleteItem

--- src/api/routes/finances/index.js
+++ src/api/routes/finances/index.js
@@ -2,7 +2,27 @@ import HttpConfig from "../../httpConfig";
 
 export const historicFinances = async () => {
   try {
-    const { data } = await HttpConfig.get("history");
+    const { data } = await HttpConfig.get("historic");
+
+    return data;
+  } catch (error) {
+    console.log("🚀 ~ file: index.js:12 ~ authLogin ~ error:", error);
+  }
+};
+
+export const insertItem = async (item) => {
+  try {
+    const { data } = await HttpConfig.post("historic", item);
+
+    return data;
+  } catch (error) {
+    console.log("🚀 ~ file: index.js:12 ~ authLogin ~ error:", error);
+  }
+};
+
+export const deleteItem = async (item) => {
+  try {
+    const { data } = await HttpConfig.delete(`historic/${item}`);
 
     return data;
   } catch (error) {
