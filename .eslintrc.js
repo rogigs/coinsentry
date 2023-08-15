7571dd026b8f8ef5039cddfa301c842e1cc6a654
Igor Soares <igorgomes.13ig@gmail.com>
2023-08-15 16:04:41 -0300

    chore: add config of lints

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  extends: ['airbnb', 'prettier'],
+  parser: '@babel/eslint-parser',
+  plugins: ['react', 'prettier', 'eslint-plugin-only-warn'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    babelOptions: {
+      parserOpts: {
+        plugins: ['jsx'],
+      },
+    },
+  },
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', 'jsx'],
+      },
+    ],
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    'import/prefer-default-export': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['src'],
+      },
+    },
+  },
+};
