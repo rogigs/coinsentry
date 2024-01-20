da34e92fdb0f04b26e8820f39beee5110187f1f6
Igor Soares <igor.soares.developer@gmail.com>
2024-01-20 03:59:11 -0300

    feat: add property lazy loading in Logo Image

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,5 +3,11 @@ import Image from 'next/image';
 import LogoImage from '../../assets/images/logo.png';
 
 export const Logo = ({ ...props }) => (
-  <Image src={LogoImage} alt="logo" className="logo" {...props} />
+  <Image
+    src={LogoImage}
+    alt="logo"
+    className="logo"
+    loading="lazy"
+    {...props}
+  />
 );
