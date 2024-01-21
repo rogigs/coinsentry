a74bee05ed7c202fae0cf3baa88123dabfc46a7d
Igor Soares <igor.soares.developer@gmail.com>
2024-01-21 18:49:20 -0300

    feat: add env NEXT_PUBLIC_SERVER_CLIENT

--- src/services/httpConfig.ts
+++ src/services/httpConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const backendUrl = 'http://localhost:4000/api'; // TODO: put this in a .env
+const backendUrl = process.env.NEXT_PUBLIC_SERVER_CLIENT;
 
 const withoutToken = axios.create({
   baseURL: `${backendUrl}`,
